e81fee313ea296bd7c15d4f78baf52a1c053917d
Christian Engen <christian.engen@schibsted.no>
2017-11-03 18:14:04 +0100

    init

---
+++ bots/randombot.js
@@ -0,0 +1,43 @@
+function doSomethingRandom(){
+    var commands = [
+        "rotate-right",
+        "rotate-left",
+        "advance",
+        "retreat",
+        "shoot"
+    ];
+    var rnd = Math.floor(Math.random() * 5);
+
+    return commands[rnd];
+}
+
+function info(){
+    return {
+        name: "Mr. Randombird",
+        team: "The best team"
+    }
+}
+
+function action (body){
+    return {
+            command: doSomethingRandom()
+            };
+}
+
+function getBody(req){
+    switch(req.method){
+        case 'GET':
+            return info();
+        case 'POST':
+            return action(req);
+    }
+}
+
+module.exports = function (context, req) {
+
+    context.log('JavaScript HTTP trigger function processed a request.', req.body);
+    context.res = {
+        body: getBody(req)
+    };
+    context.done();
+};
