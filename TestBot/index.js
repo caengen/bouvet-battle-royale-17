1db70c54edf9b2a3259f57a1d98792b019a3734d
Ali Ahmad <aliahm@nve.no>
2017-11-04 09:24:07 +0100

    debugging

--- TestBot/index.js
+++ TestBot/index.js
@@ -107,7 +107,11 @@ enum Element = {
 // TODO Find first enemy, return enemy
 function findEnemyInRange(body) {
     var result = null;
-    console.log(body);
+    console.log.error(body);
+    if (!body.enemies){
+        return;
+    }
+
     body.enemies.forEach(function (enemy) {
         if (enemy.x && !result){ // i synsfelt
             result = enemy;
